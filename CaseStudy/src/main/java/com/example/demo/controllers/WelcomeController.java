9ce156cedeca938243656615e23a5e3a86db342b
Admin <Admin@DESKTOP-F8V312A>
2021-07-12 19:03:41 +0530

    assignment

---
+++ CaseStudy/src/main/java/com/example/demo/controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value="/")
+	public String init(Model model) {
+		model.addAttribute("heading","Car details");
+		return "index";
+	}
+
+}
